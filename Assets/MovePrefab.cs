e70a0ed87fea72deb4eb777926873093885efca2
Suppe <Suppe04@gmail.com>
2022-11-09 10:58:02 +0100

    Nyt build

--- Assets/MovePrefab.cs
+++ Assets/MovePrefab.cs
@@ -14,21 +14,25 @@ public class MovePrefab : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if(isLocalPlayer)
         {
-            transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+            }
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-        }
+
     }
 }
